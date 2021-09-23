618e2a76cbee50705921723fcd4890db42ff00eb
LAPTOP-CJH <morethanever153me@outlook.com>
2021-09-23 23:51:50 +0800

    测试使用notepad++编写commit message

--- test/ATest.java
+++ test/ATest.java
@@ -1,5 +1,10 @@
 public class ATest {
+    public static String getType(Object o){ //获取变量类型方法
+        return o.getClass().toString(); //使用int类型的getClass()方法
+    }
+
     public static void main(String[] args) {
         System.out.println(1<<4);
+        System.out.println(getType('1'-'0'));
     }
 }
