0fbafe4885b4b583aa8b1571ad953d8673194bdd
Users188 <43400284+Users188@users.noreply.github.com>
2021-09-29 00:31:03 +0800

    leetcode:无重复字符的两个单词长度的最大乘积

--- src/Divide.java
+++ src/Divide.java
@@ -59,4 +59,27 @@ public class Divide {
         return negative == 1 ? -result : result;
     }
 
+    // 时间复杂度：O(1)
+    public int divide_ultimate(int a, int b) {
+        if (a == Integer.MIN_VALUE && b == -1)
+            return Integer.MAX_VALUE;
+        int sign = (a > 0) ^ (b > 0) ? -1 : 1;
+        a = Math.abs(a);
+        b = Math.abs(b);
+        int res = 0;
+        for (int i = 31; i >= 0; i--) {
+            // 首先，右移的话，再怎么着也不会越界
+            // 其次，无符号右移的目的是：将 -2147483648 看成 2147483648
+
+            // 注意，这里不能是 (a >>> i) >= b 而应该是 (a >>> i) - b >= 0
+            // 这个也是为了避免 b = -2147483648，如果 b = -2147483648
+            // 那么 (a >>> i) >= b 永远为 true，但是 (a >>> i) - b >= 0 为 false
+            if ((a >>> i) - b >= 0) { // a >= (b << i)
+                a -= (b << i);
+                res += (1 << i);
+            }
+        }
+        // bug 修复：因为不能使用乘号，所以将乘号换成三目运算符
+        return sign == 1 ? res : -res;
+    }
 }
---
+++ src/MaxProduct.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+/*leetcode:单词长度的最大乘积*/
+public class MaxProduct {
+    public int maxProduct_primitive(String[] words){
+        //暴力破解法
+        int maxproduct = 0;
+        for(int i = 0; i< words.length-1; i++){
+            for (int j = i+1; j<words.length ;j++){
+                if (!hasSameChar_primitive(words[i],words[j]))
+                    maxproduct = Math.max(maxproduct,words[i].length()*words[j].length());
+            }
+        }
+        return maxproduct;
+    }
+
+    private boolean hasSameChar_primitive(String word1,String word2){
+        for (char c:word1.toCharArray()){
+            //java字符串中查找字符，若indexOf方法找不到则返回-1
+            if (word2.indexOf(c)!= -1)
+                return true;
+        }
+        return false;
+    }
+
+    public int maxProduct_ultimate(String []words){
+        int maxproduct = 0 ;
+        int[] bitMasks= new int [words.length];
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words[i].length(); j++) {
+                //一个单词由26个小写字母组成（题意），每个单词可能出现或不出现，抽象为26位数由低到高分别表示a-z是否出现，出现为1，不出现为0。
+                //tip：类比子网掩码
+                bitMasks[i] |=(1<<words[i].charAt(j)-'a');
+            }
+        }
+        for (int i = 0; i < bitMasks.length-1; i++) {
+            for (int j = i+1; j < bitMasks.length; j++) {
+                //如果两个单词没有重复字符，则每位进行与运算时不会有1&1=1，运算结果恒为0
+                if ((bitMasks[i]&bitMasks[j])==0)
+                    maxproduct = Math.max(maxproduct,words[i].length()*words[j].length());
+            }
+        }
+        return maxproduct;
+    }
+}
