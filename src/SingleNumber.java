6c0d80a5a928cf97f8eb6f69ba99fb43de788398
LAPTOP-CJH <morethanever153me@outlook.com>
2021-10-03 00:06:46 +0800

    update SingleNumber

--- src/SingleNumber.java
+++ src/SingleNumber.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 /*leetcode:只出现一次的数字*/
 public class SingleNumber {
@@ -7,8 +7,8 @@ public class SingleNumber {
         //使用排序
 //        QuickSort.quickSort(nums);
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        int begin =0,end= nums.length-1;
+//        System.out.println(Arrays.toString(nums));
+        /*int begin =0,end= nums.length-1;
         while (begin<end){
             if (nums[begin+1] == nums[begin])
                 begin+=3;
@@ -19,6 +19,73 @@ public class SingleNumber {
             else
                 return nums[end];
         }
-        return nums[begin];
+        return nums[begin];*/
+        int left=0,right=2;
+        while (right<nums.length){
+            if (nums[left]!=nums[right])
+                return nums[left];
+            left+=3;
+            right=left+2;
+        }
+        return nums[left];
     }
+
+    public int singleNumber_modified(int []nums){
+        //利用hashmap
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int num : nums) {
+            if (!hashMap.containsKey(num))
+                hashMap.put(num, 1);
+            else{
+                int occurrences = hashMap.get(num);
+                hashMap.put(num,++occurrences);
+            }
+        }
+        //通过map.entrySet()获得键值对，性能较高
+        for (Map.Entry<Integer,Integer> entry: hashMap.entrySet()){
+            if (entry.getValue()==1)
+                return entry.getKey();
+        }
+        return 0;
+    }
+
+
+    public  int singleNumber_ultimate(int []nums){
+        //位运算
+        /*int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            int cnt = 0;
+            for (int num : nums) {
+                cnt += num >> i & 1;
+            }
+            if (cnt % 3 != 0) {
+                ret |= 1 << i;
+            }
+        }
+        return ret;*/
+
+        //有限状态机,三进制
+        int ones = 0, twos = 0;
+        for(int num : nums){
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
+
+    public int singleNumber(int []nums){
+        //集合+数学运算
+        Set<Integer> set = new HashSet<>();
+        int sum =0,sum3=0,res=0;
+        for (int num:nums){
+            sum+=num;
+            set.add(num);
+        }
+        for (Integer item:set){
+            sum3+=item;
+        }
+        //容易越界
+        return res =sum - (sum - sum3)*3/2;
+    }
+
 }
--- test/SingleNumberTest.java
+++ test/SingleNumberTest.java
@@ -1,11 +1,12 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions.*;
 
 public class SingleNumberTest {
     @Test
     public void singleNumberTest(){
         int []nums= {2,2,3,2};
         int ans = 3;
-        assertEquals(ans,new SingleNumber().singleNumber_primitive(nums));
+        Assertions.assertEquals(ans,new SingleNumber().singleNumber_primitive(nums));
     }
 }
