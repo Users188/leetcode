c77b9657ad0f3a795b9fa26231bd9749c8325557
Users188 <43400284+Users188@users.noreply.github.com>
2021-10-10 01:26:11 +0800

    滑动窗口解法

---
+++ test/MinSubArrayLenTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MinSubArrayLenTest {
+    @Test
+    public void minSubArrayLenTest(){
+        int[] nums = {2,3,1,2,4,3};
+        int target = 3;
+        Assertions.assertEquals(1,new MinSubArrayLen().minSubArrayLen(target,nums));
+    }
+}
