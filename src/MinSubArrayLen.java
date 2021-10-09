8ea901d790d4f7efd0e72276466c389ea4895c57
Users188 <43400284+Users188@users.noreply.github.com>
2021-10-10 01:27:15 +0800

    update compiled code

--- src/MinSubArrayLen.java
+++ src/MinSubArrayLen.java
@@ -9,16 +9,19 @@ import java.util.Arrays;
 public class MinSubArrayLen {
     public int minSubArrayLen(int target,int[] nums){
         //滑动窗口
-        int result = 0;
-        int left=0,right=nums.length-1;
-        while (left<right){
-            int sum=0;
-            for (int i = left; i <= right; i++) {
-                sum += nums[i];
-                if (sum>=target)
-                    break;
+        /*窗口的大小需要根据题目条件进行调整
+        最大连续...尽量扩张右边界，直到不满足题意再收缩左边界
+        最小连续...尽量缩小左边界，直到不满足题意再扩大右边界*/
+        int left=0,right=0,sum=0,result=Integer.MAX_VALUE;
+        while (right<nums.length){
+            sum+=nums[right];
+            while (sum>=target){
+                result = Math.min(result,right-left+1);
+                sum-=nums[left];
+                left++;
             }
+            right++;
         }
-        return result;
+        return result>nums.length?0:result;
     }
 }
