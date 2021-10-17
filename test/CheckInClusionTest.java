4953012b90bc8478a4aad5de64408a16656e4eb3
LAPTOP-CJH <morethanever153me@outlook.com>
2021-10-17 15:31:36 +0800

    leetcode：字符串中的变位词

---
+++ test/CheckInClusionTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CheckInClusionTest {
+    @Test
+    public void checkInClusionTest(){
+        String s1="ab";
+        String s2="eidbaooo";
+        boolean ans=true;
+        Assertions.assertEquals(ans,new CheckInClusion().checkInClusion(s1,s2));
+    }
+}
