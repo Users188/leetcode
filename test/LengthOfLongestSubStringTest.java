19bb76340430c605cd92c4099f4e4369d555a450
Users188 <43400284+Users188@users.noreply.github.com>
2021-10-19 23:20:24 +0800

    leetcode:不含重复字符的最长子字符串

---
+++ test/LengthOfLongestSubStringTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LengthOfLongestSubStringTest {
+    @Test
+    public void lengthOfLongestSubStringTest(){
+        String s="ab";
+        int ans=2;
+        Assertions.assertEquals(ans,new LengthOfLongestSubString().lengthOfLongestSubString(s));
+    }
+
+}
