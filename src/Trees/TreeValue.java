38b9f53fd9a7d17cc69f4092d7815b4814c0243e
LAPTOP-CJH <morethanever153me@outlook.com>
2021-11-17 16:35:19 +0800

    leetcode:二叉树最底层最左边的值

--- src/Trees/TreeValue.java
+++ src/Trees/TreeValue.java
@@ -1,8 +1,9 @@
 package Trees;
-/*leetcode:二叉树每层的最大值*/
+
 import java.util.*;
 
-public class LargestValues {
+public class TreeValue {
+    /*leetcode:二叉树每层的最大值*/
     public List<Integer> largestValues(TreeNode root){
         List<Integer> res = new ArrayList<>();
         if (root==null)
@@ -27,5 +28,24 @@ public class LargestValues {
             res.add(max);
         }
         return res;
+    }
+
+    /*leetcode:二叉树最底层最左边的值*/
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> levelNodes= new LinkedList<>();
+        levelNodes.add(root);
+        TreeNode ret=root;
+        while (!levelNodes.isEmpty()){
+            ret=levelNodes.peek();
+            int count=levelNodes.size();
+            while (count-- > 0){
+                TreeNode head=levelNodes.poll();
+                if (head.left!=null)
+                    levelNodes.add(head.left);
+                if (head.right!=null)
+                    levelNodes.add(head.right);
+            }
+        }
+        return ret.val;
     }
 }
