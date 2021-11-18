9862b8a19b13c3e4e3ac2ac95560326a74208095
LAPTOP-CJH <morethanever153me@outlook.com>
2021-11-18 20:34:52 +0800

    删除创建二叉树的方法

--- src/Trees/Tree.java
+++ src/Trees/Tree.java
@@ -3,19 +3,7 @@ package Trees;
 import java.util.List;
 
 public class Tree {
-    static TreeNode createBinaryTree(List<Integer> values) {
-        if (values == null || values.isEmpty()) {//最简单问题
-            return null;
-        }
-        TreeNode root = null;
-        Integer value = values.remove(0);
-        if (value != null) {
-            root = new TreeNode(value);
-            root.left = createBinaryTree(values);
-            root.right = createBinaryTree(values);
-        }
-        return root;
-    }
+
 
     /*leetcode:二叉树剪枝*/
     /*给定一个二叉树 根节点root，树的每个节点的值要么是 0，要么是 1。
