25ec08a73d22d6b7e771003f1563107fb8ccbdfc
LAPTOP-CJH <morethanever153me@outlook.com>
2021-11-18 15:17:46 +0800

    leetcode:二叉树剪枝

--- src/Trees/Tree.java
+++ src/Trees/Tree.java
@@ -16,4 +16,19 @@ public class Tree {
         }
         return root;
     }
+
+    /*leetcode:二叉树剪枝*/
+    /*给定一个二叉树 根节点root，树的每个节点的值要么是 0，要么是 1。
+    请剪除该二叉树中所有节点的值为 0 的子树。
+    节点 node 的子树为node 本身，以及所有 node的后代。*/
+    public TreeNode pruneTree(TreeNode root){
+        //后序遍历
+        if (root==null)
+            return null;
+        root.left=pruneTree(root.left);
+        root.right=pruneTree(root.right);
+        if (root.val==0 && root.left==null && root.right==null)
+            root=null;
+        return root;
+    }
 }
