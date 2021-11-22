cd7da1fca80590c372c5c2e741a539c545e02e0e
LAPTOP-CJH <morethanever153me@outlook.com>
2021-11-22 15:38:21 +0800

    leetcode:二叉树搜索树中的中继后序

--- src/Trees/Tree.java
+++ src/Trees/Tree.java
@@ -1,9 +1,9 @@
 package Trees;
 
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 
 public class Tree {
 
@@ -40,12 +40,55 @@ public class Tree {
         return order.get(0);
     }
 
-    private void inorder(TreeNode root,List<TreeNode> order){
+    private void inorder(TreeNode root, List<TreeNode> order) {
         //中序遍历
-        if (root==null)
+        if (root == null)
             return;
-        inorder(root.left,order);
+        inorder(root.left, order);
         order.add(root);
-        inorder(root.right,order);
+        inorder(root.right, order);
+    }
+
+    /*leetcode:二叉树搜索树中的中继后序*/
+    /*给定一棵二叉搜索树和其中的一个节点 p ，找到该节点在树中的中序后继。如果节点没有中序后继，请返回 null 。
+    节点p的后继是值比p.val大的节点中键值最小的节点，即按中序遍历的顺序节点 p 的下一个节点。*/
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode pointer = root;
+        boolean flag = false;
+        while (pointer != null || !stack.isEmpty()) {
+            while (pointer != null) {
+                stack.push(pointer);
+                pointer = pointer.left;
+            }
+            if (!stack.isEmpty()) {
+                pointer = stack.pop();
+                if (flag)
+                    return pointer;
+                //如果找到了节点p，将flag设为true，会返回其中继后序
+                if (pointer == p)
+                    flag = true;
+                pointer = pointer.right;
+            }
+        }
+        return null;
+    }
+
+    public TreeNode inorderSuccessor_ultimate(TreeNode root, TreeNode p) {
+        //上面的解法没有利用好二叉搜索树的性质，即L<D<R，现利用该性质
+        TreeNode pointer = root;
+        TreeNode res = null;
+        //节点p的后继是值比p.val大的节点中键值最小的节点
+        while (pointer != null) {
+            //如果当前节点值大于p，则搜索其左子树（比当前节点小）,并将返回节点设为当前节点
+            if (pointer.val > p.val) {
+                res = pointer;
+                pointer = pointer.left;
+            }
+            //否则则搜索其右子树
+            else
+                pointer = pointer.right;
+        }
+        return res;
     }
 }
